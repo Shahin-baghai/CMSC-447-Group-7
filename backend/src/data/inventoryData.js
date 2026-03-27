663dce44b59ed064a70f49207c1e1f7d0d1af915
Hamaad <hamaada2005@gmail.com>
2026-03-27 15:17:32 -0400

    move inventory data into separate data file

---
+++ backend/src/data/inventoryData.js
@@ -0,0 +1,22 @@
+const inventoryItems = [
+  {
+    slotId: "A2",
+    productName: "Flash Drive",
+    price: 5.0,
+    quantity: 3,
+    capacity: 8,
+    status: "Low Stock",
+    backstock: 12
+  },
+  {
+    slotId: "D0",
+    productName: "Tissues",
+    price: 1.0,
+    quantity: 10,
+    capacity: 10,
+    status: "In Stock",
+    backstock: 20
+  }
+];
+
+module.exports = inventoryItems;
