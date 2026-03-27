9a13e2194cb2b6da413a34e4bdf633e4166f8adf
Hamaad <hamaada2005@gmail.com>
2026-03-27 14:53:20 -0400

    backend server is setup and running, Initial inventory route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,39 @@
 const express = require("express");
 
 const app = express();
-const PORT = 5000;
+const PORT = 3001;
 
-// Middleware
 app.use(express.json());
 
-// Test route
 app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+  res.send("Backend is running");
 });
 
-// Start server
+app.get("/inventory", (req, res) => {
+  res.json({
+    items: [
+      {
+        slotId: "A2",
+        productName: "Flash Drive",
+        price: 5.0,
+        quantity: 3,
+        capacity: 8,
+        status: "Low Stock",
+        backstock: 12
+      },
+      {
+        slotId: "D0",
+        productName: "Tissues",
+        price: 1.0,
+        quantity: 10,
+        capacity: 10,
+        status: "In Stock",
+        backstock: 20
+      }
+    ]
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
